1e4e04473984c14bc71f12554545645b0bc4dad0
Sherin Olivia <sochronicle@gmail.com>
2024-04-25 15:13:15 +0700

    feat: database connection

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
+from core.models.database import SessionLocal, engine
 
 app = FastAPI()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+        
 
 @app.get("/")
 def project_root():
