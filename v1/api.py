68fd826ea693bebbb8eee280100fb75d20048eee
Sherin Olivia <sochronicle@gmail.com>
2024-04-27 18:01:02 +0700

    feat: added schedules router to v1

--- v1/api.py
+++ v1/api.py
@@ -1,7 +1,8 @@
 from fastapi import APIRouter
-from v1.endpoints import doctors, users
+from v1.endpoints import doctors, schedules, users
 
 router = APIRouter()
 
 router.include_router(users.userRouter, prefix="/users", tags=["users"])
 router.include_router(doctors.doctorRouter, prefix="/doctors", tags=["doctors"])
+router.include_router(schedules.scheduleRouter, prefix="/schedules", tags=["schedules"])
