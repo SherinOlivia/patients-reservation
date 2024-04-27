29d3912c2d13960ed088ec0332646247519e2668
Sherin Olivia <sochronicle@gmail.com>
2024-04-27 23:03:33 +0700

    fix: updated reservations table model (removed date, start time and end time, added created_at)

--- core/models/reservations.py
+++ core/models/reservations.py
@@ -1,5 +1,5 @@
 
-from sqlalchemy import Column, Date, Enum, Integer, Time, ForeignKey
+from sqlalchemy import Column, Enum, Integer, Time, ForeignKey
 from sqlalchemy.orm import relationship
 from core.models.database import Base
 from core.schemas.reservations import ReservationStatus
@@ -8,13 +8,11 @@ class Reservation(Base):
     __tablename__ = 'reservations'
 
     id = Column(Integer, primary_key=True)
-    date = Column(Date, nullable=False)
-    start_time = Column(Time(timezone=True), nullable=False)
-    end_time = Column(Time(timezone=True), nullable=False)
-    queue_number = Column(Integer, nullable=False)
-    status = Column(Enum(ReservationStatus), default=ReservationStatus.ongoing, nullable=False)
     user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     schedule_id = Column(Integer, ForeignKey('schedules.id'), nullable=False)
+    queue_number = Column(Integer, nullable=True)
+    status = Column(Enum(ReservationStatus), default=ReservationStatus.ongoing, nullable=False)
+    created_at = Column(Time(timezone=True), nullable=False)
 
     user = relationship("User", back_populates="reservations")
     schedule = relationship("Schedule", back_populates="reservation")
